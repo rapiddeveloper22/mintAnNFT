a4c76cf002344a1743170059859135e76bdcca8d
rapiddeveloper22 <mmaswin22@gmail.com>
2022-03-04 15:03:22 +0530

    done

--- bs-config.js
+++ bs-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: 3000,
+    port: process.env.PORT || 3000,
     files: ['.src/**/*.{html,htm,css,js}'],
     server: {
         baseDir: ["./src", "frontend", "./build/contracts"]
--- frontEnd/nodejs.js
+++ frontEnd/nodejs.js
@@ -6,8 +6,9 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/" + "index.html");
 })
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at http://%s:%s//", host, port);
+const hostt = '0.0.0.0';
+const portt = process.env.PORT || 3000;
+
+app.listen(portt, hostt, function () {
+    console.log("Server started.......");
 });
